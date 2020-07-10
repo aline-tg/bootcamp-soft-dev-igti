b5706fc96f2cd18a9e0c443abd7973ad66409155
aline <alinetgue@gmail.com>
2020-07-09 23:08:11 -0300

    Add new files

---
+++ scripts-test/operators.js
@@ -0,0 +1,44 @@
+var sum = 4 + 2
+var diff = 5 - 3
+var prod = 5 * 4
+var div = 20 / 4
+var rest = 20 % 3
+
+console.log("sum:" + sum)
+console.log("diff:" + diff)
+console.log("prod:" + prod)
+console.log("div:" + div)
+console.log("rest:" + rest)
+
+var num1 = 10
+var num2 = 50
+var result = (num2 + num1)/ (8 + 2)
+console.log("result: " + result)
+
+var increment = 1
+increment = increment + 1
+increment++
+console.log("increment:" + increment)
+
+var aux = 5
+aux += 6 //the same a + 6
+console.log(aux)
+
+var reduce = 5
+reduce = reduce - 1
+reduce--
+console.log(reduce)
+
+var aux2 = 10
+aux2 -= 4
+console.log(aux2)
+
+var aux3 = 4
+aux3 *= 5 
+console.log(aux3)
+
+var aux4 = 20
+aux4 /= 5 
+console.log(aux4)
+
+var 

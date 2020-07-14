dbd768ea6d97bf93dd49d3052c33dede5d576db6
aline <alinetgue@gmail.com>
2020-07-14 12:03:25 -0300

    Added some test files

---
+++ scripts-test/webpage-tests/index.js
@@ -0,0 +1,6 @@
+//Script example to learn how to create a web page
+// that uses js+css+html
+//Created in 2020-07-14 by Aline Guerreiro
+
+console.log("Hello World in external file")
+document.write("Any text")

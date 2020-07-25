f61656b2dfc678805118978b058915fd1b65b729
aline <alinetgue@gmail.com>
2020-07-25 16:17:44 -0300

    Added some Java test codes

---
+++ mod2/test-examples/project1-test/src/firstProgramPackage/firstProgramPackageClass.java
@@ -0,0 +1,8 @@
+
+package firstProgramPackage;
+
+public class firstProgramPackageClass {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
---
+++ mod2/test-examples/project2-class1_6/src/examplePackage/ClassOneTest.java
@@ -0,0 +1,10 @@
+package examplePackage;
+
+public class ClassOneTest {
+    public static void main(String[] args) {
+        int age;
+        age = 25;
+
+        System.out.print("I'm "+ age +" years old");
+    }
+}

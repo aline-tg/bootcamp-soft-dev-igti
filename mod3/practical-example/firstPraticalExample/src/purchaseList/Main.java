c8b340b98cb28d1f488673aac464dd13656da4a4
aline <alinetgue@gmail.com>
2020-08-09 18:28:20 -0300

    Added new files

---
+++ mod3/practical-example/firstPraticalExample/src/purchaseList/Main.java
@@ -0,0 +1,11 @@
+//Script of list examples from module 3 in Java
+//Created in 2020-08-08 by Aline Guerreiro
+
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args){
+        PurchaseList purchaseList = new PurchaseList();
+        purchaseList.start();
+    }
+}
